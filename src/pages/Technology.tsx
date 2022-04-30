b2919b1cee54c7ac3647cd491d7a780e2394bf05
Adam Kula <adamkula97@gmail.com>
2022-04-30 20:32:17 +0200

    technology page responsive dizajn

--- src/pages/Technology.tsx
+++ src/pages/Technology.tsx
@@ -1,39 +1,56 @@
 import { useState } from 'react'
 import styled from "styled-components";
 import Navigation from '../components/Navigation'
-import BgDestination from '../assets/destination/background-destination-desktop.jpg'
+import BgTechnology from '../assets/technology/background-technology-desktop.jpg'
+import BgTechnologyDevice from '../assets/technology/background-technology-tablet.jpg'
 import data from '../data.json'
 
 const Wrapper = styled.div`
-    background-image: url(${BgDestination});
+    background-image: url(${BgTechnology});
     background-position: center;
     background-size: cover;
     background-attachment: fixed;
     min-height: 100vh;
     transition: .5s ease;
     background-color: #161515;
+    @media only screen and (max-width: 1220px) {
+    background-image: url(${BgTechnologyDevice});
+  }
 `;
 
 const Main = styled.main`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-between;
+  padding-left: 13%;
+  gap: 5rem;
   align-items: center;
   color: #fafafa;
+  @media only screen and (max-width: 1400px) {
+    padding-left: 2%;
+    gap: 0;
+  }
+  @media only screen and (max-width: 1024px) {
+    flex-direction: column-reverse;
+    padding-left: 0;
+  }
 `;
 
-const Content = styled.section`
-  width: 615px;
-`;
+const Content = styled.section``;
 
 const PreSection = styled.aside`
   display: flex;
   gap: 4rem;
-  height: 344px;
+  @media only screen and (max-width: 1220px) {
+    gap: 2rem;
+  }
+  @media only screen and (max-width: 1024px) {
+    flex-direction: column;
+    text-align: center;
+  }
 `;
 
 const PostTitle = styled.h5`
-  max-width: 1390px;
   margin: auto;
   font-size: 28px;
   font-family: 'Barlow Condensed';
@@ -42,6 +59,29 @@ const PostTitle = styled.h5`
   font-weight: 300;
   color: #fafafa;
   margin-top: 5rem;
+  padding-left: 13%;
+  @media only screen and (max-width: 1400px) {
+    padding-left: 2%;
+  }
+  @media only screen and (max-width: 1220px) {
+    margin-bottom: 4rem;
+  }
+  @media only screen and (max-width: 1024px) {
+    padding-left: 2rem;
+    margin: 4rem 0;
+  }
+  @media only screen and (max-width: 768px) {
+    margin: 3rem 0;
+    font-size: 20px;
+  }
+  @media only screen and (max-width: 520px) {
+    margin: 2.5rem 0;
+    text-align: center;
+    padding-left: 0;
+  }
+  @media only screen and (max-width: 375px) {
+    font-size: 16px;
+  }
 `;
 
 const Title = styled.h3`
@@ -50,6 +90,15 @@ const Title = styled.h3`
   font-weight: 400;
   text-transform: uppercase;
   margin: 0;
+  @media only screen and (max-width: 1024px) {
+    padding-bottom: 1rem;
+  }
+  @media only screen and (max-width: 768px) {
+    font-size: 40px;
+  }
+  @media only screen and (max-width: 375px) {
+    font-size: 24px;
+  }
 `;
 
 const List = styled.ul`
@@ -58,6 +107,15 @@ const List = styled.ul`
   justify-content: space-evenly;
   gap: var(--gap, 2rem);
   margin: 0;
+  @media only screen and (max-width: 1024px) {
+    flex-direction: row;
+    gap: 2rem;
+    justify-content: center;
+    margin-top: 4rem;
+  }
+  @media only screen and (max-width: 520px) {
+    margin-top: 2rem;
+  }
 `;
 
 const ListItem = styled.li`
@@ -82,6 +140,14 @@ const Link = styled.a`
     background-color: #fafafa;
     color: #0B0D17;
   }
+  @media only screen and (max-width: 768px) {
+    padding: 4px;
+  }
+  @media only screen and (max-width: 520px) {
+    width: 40px;
+    height: 40px;
+    font-size: 20px;
+  }
 `;
 
 const Span = styled.span`
@@ -96,6 +162,18 @@ const Text = styled.p`
   font-family: 'Barlow';
   line-height: 32px;
   font-weight: 100;
+  @media only screen and (max-width: 1024px) {
+    margin: auto;
+    max-width: 480px;
+  }
+  @media only screen and (max-width: 768px) {
+    font-size: 16px;
+    max-width: 430px;
+  }
+  @media only screen and (max-width: 520px) {
+    font-size: 15px;
+    padding: 0 2rem;
+  }
 `;
 
 const Desc = styled.p`
@@ -104,11 +182,35 @@ const Desc = styled.p`
   letter-spacing: 2.7px;
   color: #D0D6F9;
   text-transform: uppercase;
+  @media only screen and (max-width: 520px) {
+    font-size: 14px;
+  }
 `;
 
-const Wrap = styled.div``;
+const Wrap = styled.div`
+  @media only screen and (max-width: 1024px) {
+    margin: auto;
+  }
+`;
 const Container = styled.section``;
-const Img = styled.img``;
+const Img = styled.img`
+  @media only screen and (max-width: 1220px) {
+    width: 380px;
+  }
+  @media only screen and (max-width: 1024px) {
+    display: none;
+  }
+`;
+
+const ImgDevice = styled.img`
+  @media only screen and (min-width: 1024px) {
+    display: none;
+    width: 100%;
+  }
+  @media only screen and (max-width: 1024px) {
+    width: 100%;
+  }
+`;
 
 
 const Destination = () => {
@@ -146,6 +248,7 @@ const Destination = () => {
 
           <Container>
             <Img src={images.portrait} title={name} alt={name} />
+            <ImgDevice src={images.landscape} title={name} alt={name} />
           </Container>
       </Main>
     </Wrapper>
